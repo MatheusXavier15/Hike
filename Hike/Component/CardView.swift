b51690b8a0501e32a92ecb373e7059ec6572d202
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-07-26 00:41:29 -0300

    [Feature] Button styles

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,6 +8,22 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage(){
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        
+        
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
         // MARK: - CARD
         ZStack {
@@ -58,13 +74,35 @@ struct CardView: View {
                             )
                         )
                         .frame(width: 256, height: 256)
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.default, value: imageNumber)
                 }
                 
                 // MARK: - FOOTER
                 
+                Button {
+                    // ACTION: Generate random number
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGreenLight,
+                                    .customGreenMedium
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
+
             }
         } // CARD
         .frame(width: 320, height: 570)
