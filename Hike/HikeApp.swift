45af612a03d17853008c59f65cd6dfa71454d881
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-07-25 22:59:04 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Matheus Xavier on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
